914545bfe62073accfa6a5ab56b4e380a910e7fa
Basile Maille <basmaille59@gmail.com>
2023-07-04 16:18:36 +0200

    first commit

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import ApiService from "./service";
+
+export default function getAllFestivals() {
+    const apiService = new ApiService();
+    apiService.getFestivals().then((festivals) => {
+        return festivals;
+    });
+}
---
+++ src/service.ts
@@ -0,0 +1,16 @@
+export default class ApiService {
+    url: string;
+  
+    constructor() {
+      this.url = "https://data.culture.gouv.fr/api/records/1.0/search/?dataset=panorama-des-festivals";
+    }
+  
+    async getFestivals(params: Record<string, any> = {}): Promise<any> {
+      const query = new URLSearchParams(params).toString();
+      const requestUrl = query ? `${this.url}&${query}` : this.url;
+  
+      const response = await fetch(requestUrl);
+      return await response.json();
+    }
+  }
+  
