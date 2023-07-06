992e17ad3f7fc46c2554c69a831247919bf314ca
Basile Maille <basmaille59@gmail.com>
2023-07-06 17:33:31 +0200

    tried to add babel and make test, didn't work

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -7,3 +7,44 @@ describe('ApiService', () => {
     });
   });
   
+  // Les tests ne fonctionnent pas, je n'ai pas réussi à les faire fonctionner
+  
+  // import { getAllFestivals, getFestivalsByDepartmentCode } from '../src';
+
+  // // Mock fetch function
+  // global.fetch = jest.fn().mockResolvedValue({
+  //   json: jest.fn().mockResolvedValue({
+  //     records: [
+  //       { name: 'Festival 1', location: 'Paris' },
+  //       { name: 'Festival 2', location: 'Marseille' },
+  //     ],
+  //   }),
+  // }) as jest.Mock;
+  
+  
+  // describe('getAllFestivals', () => {
+  //   test('should return all festivals', async () => {
+  //     const festivals = await getAllFestivals();
+  //     expect(festivals).toEqual([
+  //       { name: 'Festival 1', location: 'Paris' },
+  //       { name: 'Festival 2', location: 'Marseille' },
+  //     ]);
+  //   });
+  // });
+  
+  // describe('getFestivalsByDepartmentCode', () => {
+  //   test('should return festivals for valid department code', async () => {
+  //     const departmentCode = 75; // Paris
+  //     const festivals = await getFestivalsByDepartmentCode(departmentCode);
+  //     expect(festivals).toEqual([
+  //       { name: 'Festival 1', location: 'Paris' },
+  //       { name: 'Festival 2', location: 'Marseille' },
+  //     ]);
+  //   });
+  
+  //   test('should throw an error for invalid department code', async () => {
+  //     const departmentCode = 1000; // Invalid department code
+  //     await expect(getFestivalsByDepartmentCode(departmentCode)).rejects.toThrow('Invalid department code');
+  //   });
+  // });
+  
